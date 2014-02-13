f167a2437ea10d2049bfdc45df4275502245395b
Grief-Code <hasgrief@googlemail.com>
2014-02-13 18:03:07 +0000

    hmmm this maybe^^

--- ghost++/ghost/ghost.cpp
+++ ghost++/ghost/ghost.cpp
@@ -609,6 +609,9 @@ CGHost :: CGHost( CConfig *CFG )
     MapCFG.Read( m_MapCFGPath + m_DefaultMap );
     m_Map = new CMap( this, &MapCFG, m_MapCFGPath + m_DefaultMap );
 
+    if(!m_PersistLobby)
+        LoadIPToCountryData( );
+
     m_AutoHostMap = new CMap( *m_Map );
     m_SaveGame = new CSaveGame( );
 
